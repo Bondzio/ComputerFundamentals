55ab357b8313232e0afc6a343df059d0cfad6866
Guo Junjun <guojunjun@gmail.com>
2014-10-25 23:53:30 +0200

    Age of Computers 3

--- python.py
+++ python.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 '''Age of Computers 3 '''
 
 def ds(s, r):            # Disk system
@@ -25,6 +27,11 @@ def dma(bakgrounnKrever, maskinsykler, bussen):
 def dma2(bit_per_sek, bit_per_bokstav, mips):
     print(bit_per_sek / bit_per_bokstav) / ( mips * (10.0 ** 6) )
 
+# DMA oppgave 1
+def dma1(sektorerPerSpor, diskRotererRPM, avbruddMikrosekund):
+    lesetidPerSektor = (60.0 / diskRotererRPM) / sektorerPerSpor
+    print((avbruddMikrosekund / (10.0 ** 6)) / lesetidPerSektor)
+
 # Hurtigbufferberegning
 def hurtigbuffer(L1p, L2p, L1aksesstid, snitt, Minneaksesstid):
     print(snitt - (L1p * L1aksesstid + (1 - L1p) * (1 - L2p) * Minneaksesstid)) / ((1 - L1p) * L2p)
@@ -33,3 +40,36 @@ def hurtigbuffer(L1p, L2p, L1aksesstid, snitt, Minneaksesstid):
 def operasjonstid(oppdateringssykel, oppdateringsoperasjon):
     print(oppdateringsoperasjon / ((1.0 / oppdateringssykel) * (10.0 ** 6)))
 
+# RAID: nedre grense for tidsbruk for å lese fil fra disk
+def lesedisk(omdreiningerPrSek, sektorerPrSpor, sektorstorrelseB, soketidMs, filstorrelseM):
+    print(( (filstorrelseM * 1024) / (omdreiningerPrSek * sektorerPrSpor * sektorstorrelseB)) + soketidMs / 1000)
+
+# Lesetid return sekund
+def lesetid(omdreiningerPrMinutt, sektorerPrSpor, sektorstorrelseB, soketidMs, filstorrelseM):
+    print(((filstorrelseM * (1024 ** 2)) / (
+        (omdreiningerPrMinutt / 60.0) * sektorerPrSpor * sektorstorrelseB)) + soketidMs / 1000)
+
+# Lagerstørrelse: svar kB
+def lagerstorrelse(buffer1KB, buffer2MB, primerMB, eksternminneGB):
+    print(buffer1KB + buffer2MB * 1024 + primerMB * 1024 + eksternminneGB * (1024 ** 2))
+
+# Diskaksess return ns nanosecond
+def diskaksess(hurtigbufferNS, hovedlagerNS, diskMS, hurtigbufferTreffrate, hovedlageretTreffrate):
+    search1 = hurtigbufferNS * hurtigbufferTreffrate
+    search2 = (hurtigbufferNS + hovedlagerNS) * (1 - hurtigbufferTreffrate) * hovedlageretTreffrate
+    search3 = (hurtigbufferNS + hovedlagerNS + (diskMS * (10.0 ** 6))) * (1 - hurtigbufferTreffrate) * (
+        1 - hovedlageretTreffrate)
+    print(search1 + search2 + search3)
+
+# Mikroprosessoren svare KB
+def mikroprosessoren(bitAdresser, bitDatabuss):
+    print((2 ** bitAdresser) * bitDatabuss / (1024 * 8))
+
+# Prosessor, beregning
+def prosessorBeregning(antallInstruksjonerPerKlokkesyklus, aksessLager, hurtigbufferTraffrate, aksesstid,
+                       hurtigbufferlinjeHenteFraHovedlagerByte, hurtigbufferHovedlagerBussenMHz, bussBredBit):
+    busshastighet = hurtigbufferHovedlagerBussenMHz * bussBredBit
+    prosessorKravet = (antallInstruksjonerPerKlokkesyklus / aksessLager) * (
+        1 - hurtigbufferTraffrate) * (hurtigbufferlinjeHenteFraHovedlagerByte * 8)
+    print(( busshastighet / prosessorKravet))
+    print(hurtigbufferHovedlagerBussenMHz / (antallInstruksjonerPerKlokkesyklus * (1 - hurtigbufferTraffrate)))
